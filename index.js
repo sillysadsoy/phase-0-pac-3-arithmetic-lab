de284fa8f729cf41ac7a637ac914d4227d11b83e
roger chat <mattrogerpelosi@gmail.com>
2021-07-07 15:28:06 -0400

    Assignment Complete

---
+++ index.js
@@ -0,0 +1,33 @@
+//ONE-----------------------------------
+function add(num1, num2) {
+    return num1 + num2;
+}
+//TWO-----------------------------------
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+//THREE-----------------------------------
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+//FOUR-----------------------------------
+function divide(num1, num2) {
+    return num1 / num2;
+}
+//FIVE-----------------------------------
+function increment(n) {
+    return n +=1;
+}
+//SIX-----------------------------------
+function decrement(n) {
+    return n -=1;
+}
+//SEVEN-----------------------------------
+function makeInt(string) {
+    return parseInt(string, 10);
+}
+//EIGHT-----------------------------------
+function preserveDecimal(string) {
+    return parseFloat(string);
+}
+
